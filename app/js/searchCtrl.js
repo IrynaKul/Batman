f3de8b3d962b220d6ee86e71c755657ea0406055
IrynaKul <irusja.93@gmail.com>
2016-03-28 14:53:24 +0200

    något

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,5 +1,10 @@
-batmanPlannerApp.controller('SearchCtrl', function ($scope,Batman) {
-	Batman.CharacterSearch.get(function(data){
+console.log("mjauuu");
+batmanPlannerApp.controller('SearchCtrl', function ($scope,batman) {
+  console.log("hshhsh");
+
+	batman.CharacterSearch.get(function(data){
+      console.log("i CharacterSearch");
+      console.log(data);
      $scope.dishes=data.Results;
      $scope.status = "Showing " + data.Results.length + " results";
    },function(data){
@@ -7,15 +12,15 @@ batmanPlannerApp.controller('SearchCtrl', function ($scope,Batman) {
    });
 
 
-  $scope.search = function(query) {
-   $scope.status = "Searching...";
-   console.log(query);
-   Batman.CharacterSearch.get({any_kw:query},function(data){
-     $scope.dishes=data.Results;
-     $scope.status = "Showing " + data.Results.length + " results";
-   },function(data){
-     $scope.status = "There was an error";
-   });
- }
+ //  $scope.search = function(query) {
+ //   $scope.status = "Searching...";
+ //   console.log(query);
+ //   batman.CharacterSearch.get({any_kw:query},function(data){
+ //     $scope.dishes=data.Results;
+ //     $scope.status = "Showing " + data.Results.length + " results";
+ //   },function(data){
+ //     $scope.status = "There was an error";
+ //   });
+ // }
 
-})
+});
