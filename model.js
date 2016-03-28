403214235e3a2f3d70d5aa92afbb9026e856029e
rockbjornen <bjorn_lundkvist@hotmail.com>
2016-03-28 11:42:33 +0200

    aekdsghb

--- model.js
+++ model.js
@@ -8,7 +8,7 @@ BatmanApp.factory('Batman',function ($resource) {
 	};
 
 	console.log("hej")console.log("hej")console.log("hej")
-	console.log("hej")
+	
 	
 
 	this.Character = $resource('http://comicvine.gamespot.com/api/character:id',{api_key:'bad0d77f8c20f0671597ccd9a8bcdf3b3c680fb8'});
