f1d4e6bd26462ba9685a83f2ed9d519122e27477
Björn Lundkvist <Bjrn Lundkvist>
2016-03-28 11:18:05 +0200

    test

--- model.js
+++ model.js
@@ -7,6 +7,11 @@ BatmanApp.factory('Batman',function ($resource) {
 		console.log(test)
 	};
 
+	console.log("hej")
+	console.log("hej")
+	console.log("hej")
+	console.log("hej")
+
 	this.Character = $resource('http://comicvine.gamespot.com/api/character:id',{api_key:'bad0d77f8c20f0671597ccd9a8bcdf3b3c680fb8'});
 	console.log(this.Character)
 
