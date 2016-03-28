f74a06bc473b06f418a70143751d806210063eab
Björn Lundkvist <Bjrn Lundkvist>
2016-03-28 11:04:24 +0200

    added a model

---
+++ model.js
@@ -0,0 +1,13 @@
+BatmanApp.factory('Batman',function ($resource) {
+
+	this.test();
+
+	this.test = function(){
+		var test = this.Character;
+		console.log(test)
+	};
+
+	this.Character = $resource('http://comicvine.gamespot.com/api/character:id',{api_key:'bad0d77f8c20f0671597ccd9a8bcdf3b3c680fb8'});
+	console.log(this.Character)
+
+}
