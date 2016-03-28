444a590178ccc912c54a0e9a0fd11e47e3ce7f7f
IrynaKul <irusja.93@gmail.com>
2016-03-28 11:47:59 +0200

    adderar filer

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var batmanPlannerApp = angular.module('batmanPlanner', ['ngRoute','ngResource','ngCookies']);
+
+batmanPlannerApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'partials/home.html'
+      }).
+      when('/search', {
+        templateUrl: 'partials/search.html',
+        controller: 'SearchCtrl'
+      }).
+      when('/dish/:dishId', {
+        templateUrl: 'partials/character.html',
+        controller: 'CharacterCtrl'
+      }).
+
+      // // TODO in Lab 5: add more conditions for the last two screens (overview and preparation)
+      // when('/overview',{
+      //   templateUrl: 'partials/overview.html',
+      //   controller: 'batmanCtrl'
+      // }).
+      // when('/preparation',{
+      //   templateUrl: 'partials/preparation.html',
+      //   controller: 'batmanCtrl'
+      // }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
