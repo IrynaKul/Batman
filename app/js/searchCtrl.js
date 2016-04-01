6c775e7c806fb54a56c1bdc6921f765be56f154a
IrynaKul <irusja.93@gmail.com>
2016-04-01 15:41:07 +0200

    searchfunktionen funkar

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,9 +1,24 @@
 batmanPlannerApp.controller('SearchCtrl', function ($scope,batmanModel){
- 
-  batmanModel.getTop10();
-  $scope.array= batmanModel.getArray();
+  $scope.enemies=batmanModel.setEnemiesArray();
+  $scope.top12=batmanModel.getTop12();
+  $scope.array= function(){
+    return batmanModel.getArray();
+  }
 
   $scope.getObj = function(){
-    return $scope.array;
+    return $scope.array();
   };
+
+  $scope.search = function(query) {
+   $scope.status = "Searching...";
+   console.log(query);
+   batmanModel.searchVillain(query).get(function(data){
+     $scope.villain=data.results;
+     console.log(data.results)
+     batmanModel.setFiltered(data.results,query);
+     $scope.status = "Showing " + data.results.length + " results";
+   },function(data){
+     $scope.status = "There was an error";
+   });
+ }
 });
