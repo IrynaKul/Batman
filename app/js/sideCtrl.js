eea68673870859791e0dfac0fdcfc38b5764e55d
IrynaKul <irusja.93@gmail.com>
2016-04-22 13:39:23 +0200

    fixar den konstiga sidebarCtrl, gör en ny fil sideCtrl.js

---
+++ app/js/sideCtrl.js
@@ -0,0 +1,39 @@
+batmanPlannerApp.controller('sideBarCtrl', function ($scope,$routeParams,batmanModel){
+
+
+		$("#sidebutton").click(function(){
+			console.log("activate");
+			var button_status = document.getElementById("sidebutton");
+			console.log("status", button_status);
+			if (button_status.value=="show sidebar"){
+				console.log("if");
+				button_status.value = "hide sidebar";
+				$("#sidebar").animate({
+					width: 'toggle'
+				});
+				$("#container").animate({
+					width: '70%'
+				});
+			}
+
+			else{
+				console.log("else");
+				button_status.value = "show sidebar";
+				$("#sidebar").animate({
+					width: 'toggle'
+				});
+				$("#container").animate({
+					width: '100%'
+				});
+			}
+
+		})
+		
+		
+	$scope.getUserScore=function(){
+    return batmanModel.getUserScore();
+	}
+
+
+
+});
